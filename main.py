2b8cca77fcbfec439f891bab19f022e53e3dde9b
Navin10101010101 <navindubey440@gmail.com>
2025-08-07 14:40:22 +0530

    Enable CORS for Vercel frontend

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS Setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://dreamhome-frontend.vercel.app/"],  # your frontend URL
+    allow_origins=["https://dreamhome-backend-xyc4.onrender.com"],  # your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
